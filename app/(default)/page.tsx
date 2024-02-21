73151c054218e2928e9a1d607b30e6a087d9b7a3
Tomide Adeoye <55337742+tomideadeoye@users.noreply.github.com>
2024-02-21 22:16:13 +0100

    change metadaTA

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,13 +1,13 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
-}
+  title: "MerisLabs",
+  description: "End-to-end software development for businesses.",
+};
 
-import Hero from '@/components/hero'
-import Features from '@/components/features'
-import Newsletter from '@/components/newsletter'
-import Zigzag from '@/components/zigzag'
-import Testimonials from '@/components/testimonials'
+import Hero from "@/components/hero";
+import Features from "@/components/features";
+import Newsletter from "@/components/newsletter";
+import Zigzag from "@/components/zigzag";
+import Testimonials from "@/components/testimonials";
 
 export default function Home() {
   return (
@@ -18,5 +18,5 @@ export default function Home() {
       <Testimonials />
       <Newsletter />
     </>
-  )
+  );
 }
