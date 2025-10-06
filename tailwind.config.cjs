3e9fc4f3fca49bfb6e0e9b6df0ef9e8827c976a6
Tomide Adeoye <tomideadeoye@gmail.com>
2025-10-06 09:12:55 +0100

    Rename tailwind.config.js to .cjs for CommonJS compatibility

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import tailwindcssAnimate from 'tailwindcss-animate';
-
-export default {
+module.exports = {
   darkMode: ['class'],
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -90,5 +88,5 @@ export default {
       },
     },
   },
-  plugins: [tailwindcssAnimate],
+  plugins: [require('tailwindcss-animate')],
 };
